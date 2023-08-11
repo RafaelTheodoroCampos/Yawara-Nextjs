a311505b0b5e8e956c8ca1da3be44d87655962f6
rafae <rafaeltheocc@gmail.com>
2023-08-10 23:36:17 -0300

    Feature: segurança
    .envlocal e header começando

---
+++ yawara-nextjs/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
+      <div className="text-gray-100">AAAAAAAAAAAAA</div>
+    </header>
+  );
+}
+
+export default Header;
